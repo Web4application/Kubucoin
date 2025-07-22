968aeea6951a8bd5e51f92f1edef8ba300b43e29
Web4 <167559384+Web4application@users.noreply.github.com>
2025-07-23 00:54:17 +0200

    Create bash.sh

---
+++ bash.sh
@@ -0,0 +1,18 @@
+gh repo clone OmniBTC/OmniBridge
+gh repo clone chainer/chainercv
+gh repo clone ethereum-lists/chains
+gh repo clone QUBUHUB/go-primitives.git
+Add remote https://github.com/base-org/node.git Node.git
+gh repo clone piratemoo/cowbrow
+gh repo clone Open-Rights-Exchange/chain-js
+gh repo clone QUBUHUB/Kubucoin.eth.git
+gh repo clone gochain/chainload
+gh repo clone shentufoundation/shentu
+add remote https://github.com/trustwallet/assets/tree/eb827ffe6570a49fc422c1fc7daff9b3ff44a5a0/ .github
+T
+gh repo clone UCRegistry/chain-registry
+
+let model = GenerativeModel(name: "gemini-1.0-pro-vision-latest", apiKey: "AIzaSyAvrxOyAVzPVcnzxuD0mjKVDyS2bNWfC10")
+let cookieImage = UIImage(...)
+let prompt = "Do these look store-bought or homemade?"
+let response = try await model.generateContent(prompt, cookieImage)
